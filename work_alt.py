87cf129b0c540f54224c74361e842f2ed44323de
Yaron Blinder <YaronBlinder@gmail.com>
2017-06-19 13:49:20 -0400

    added VGG19

--- work_alt.py
+++ work_alt.py
@@ -16,7 +16,7 @@ import glob
 N_classes = 3
 
 def assert_validity(args):
-    valid_models = ['resnet50', 'vgg16', 'vgg19', 'inception_v3', 'xception']
+    valid_models = ['resnet50', 'vgg16', 'vgg19']#, 'inception_v3', 'xception']
     valid_groups = [
         'F_Ped', 'M_ped',
         'F_YA', 'M_YA',
