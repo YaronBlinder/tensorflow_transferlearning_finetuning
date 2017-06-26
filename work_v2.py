8433ed725c46cf839e75c86188dd78a264aafb8d
Yaron Blinder <YaronBlinder@gmail.com>
2017-06-26 16:08:33 -0400

    changed model to waya with ELU

--- work_v2.py
+++ work_v2.py
@@ -139,12 +139,14 @@ def get_model(model, freeze_base=False):
     # x = keras.layers.Dropout(0.5)(x)
     # x = keras.layers.Dense(1024, activation="relu", kernel_initializer=glorot_normal(), trainable=True)(x)
 
-    # x = keras.layers.Dense(1024)(x)
-    # x = keras.layers.BatchNormalization()(x)
-    # x = keras.layers.advanced_activations.LeakyReLU()(x)
-    # x = keras.layers.Dropout(0.25)(x)
-    x = keras.layers.Dense(256)(x)
-    x = keras.layers.Dropout(0.5)(x)
+    x = keras.layers.Dense(1024)(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.advanced_activations.ELU()(x)
+    x = keras.layers.Dropout(0.25)(x)
+
+    # x = keras.layers.Dense(256)(x)
+    # x = keras.layers.Dropout(0.5)(x)
+
     predictions = keras.layers.Dense(N_classes, activation='sigmoid', name='class_id')(x)
 
     # predictions = keras.layers.Dense(N_classes, activation='softmax', name='class_id', trainable=True)(x)
