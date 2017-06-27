cd9afe29e9bb1ba550a9ef14f933cbb5e3190598
Yaron Blinder <YaronBlinder@gmail.com>
2017-06-27 19:59:30 -0400

    added *ception models

--- extended_keras_image.py
+++ extended_keras_image.py
@@ -330,7 +330,7 @@ def random_crop(x, random_crop_size, sync_seed=None, **kwargs):
     w, h = x.shape[0], x.shape[1]
     rangew = (w - random_crop_size[0]) // 2
     rangeh = (h - random_crop_size[1]) // 2
-    print([w,h])
+    # print([w,h])
     offsetw = 0 if rangew == 0 else np.random.randint(rangew)
     offseth = 0 if rangeh == 0 else np.random.randint(rangeh)
     return x[offsetw:offsetw + random_crop_size[0], offseth:offseth + random_crop_size[1], :]
