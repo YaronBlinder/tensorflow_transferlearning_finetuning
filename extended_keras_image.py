b748dd07beb5c3ccbdc83f604e1df84491d2ffb9
Yaron Blinder <YaronBlinder@gmail.com>
2017-06-26 22:47:52 -0400

    debugging random_crop

--- extended_keras_image.py
+++ extended_keras_image.py
@@ -310,9 +310,10 @@ def random_crop(x, random_crop_size, sync_seed=None, **kwargs):
     w, h = x.shape[1], x.shape[2]
     rangew = (w - random_crop_size[0]) // 2
     rangeh = (h - random_crop_size[1]) // 2
+    print([w, h, rangew, rangeh])
     offsetw = 0 if rangew == 0 else np.random.randint(rangew)
     offseth = 0 if rangeh == 0 else np.random.randint(rangeh)
-    return x[:, offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1]]
+    return x[offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1], :]
 
 def random_transform(x,
                      dim_ordering='th',
