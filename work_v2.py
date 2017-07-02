32e025ea56eee4b4aca7622a8496972025943da1
Yaron Blinder <YaronBlinder@gmail.com>
2017-07-02 16:24:42 -0400

    preparing for PA_LAT run

--- work_v2.py
+++ work_v2.py
@@ -237,8 +237,8 @@ def train_top(model, top, group, position, n_epochs):
         # train_path = 'data/{position}/train_318/{group}/train/'.format(position=position, group=group)
         # test_path = 'data/{position}/train_318/{group}/test/'.format(position=position, group=group)
     else:
-        train_path = 'data/{position}_224/{group}/train/'.format(position=position, group=group)
-        test_path = 'data/{position}_224/{group}/test/'.format(position=position, group=group)
+        train_path = 'data/{position}_256/{group}/train/'.format(position=position, group=group)
+        test_path = 'data/{position}_256/{group}/test/'.format(position=position, group=group)
         # train_path = 'data/{position}/train_256_3ch_flip/{group}/train/'.format(position=position, group=group)
         # test_path = 'data/{position}/train_256_3ch_flip/{group}/test/'.format(position=position, group=group)
 
