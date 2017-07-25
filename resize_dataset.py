eb6af24c4d756e46b3c82ed6b979123533232e6d
Yaron Blinder <YaronBlinder@gmail.com>
2017-07-24 23:27:03 -0400

    299 16bit

---
+++ resize_dataset.py
@@ -0,0 +1,24 @@
+import os
+import png
+from cv2 import imread, resize
+from tqdm import tqdm
+
+data_path = '~/Radical/data/PA_512_16/M_Adult/'
+resize_path = '~/Radical/data/PA_299_16/M_Adult/'
+
+dirs = ['train', 'test']
+labels = ['1', '2']
+
+for dir in dirs:
+    for label in labels:
+        relevant_data_path = data_path + dir + '/' + label +'/'
+        relevant_resize_path = resize_path + dir + '/' + label + '/'
+        files = os.listdir(relevant_data_path)
+        for file in tqdm(files):
+            im = imread(file)
+            im = resize(im, (299, 299))
+            with open(relevant_resize_path + file, 'wb') as f:
+                writer = png.Writer(width=im.shape[1], height=im.shape[0], bitdepth=16)
+                im2list = im.reshape(-1, im.shape[1] * im.shape[2]).tolist()
+                writer.write(f, im2list)
+
