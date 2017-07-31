33c11e5ed06c7febba0f89df46009d8ee672a7dd
Yaron Blinder <YaronBlinder@gmail.com>
2017-07-31 11:35:55 -0400

    calculate dataset std

---
+++ get_std.py
@@ -0,0 +1,35 @@
+import argparse
+import glob
+import os
+from tqdm import tqdm
+
+from cv2 import imread
+
+
+def get_dataset_std(path, ds_mean):
+
+    count = 0
+
+    for r, dirs, files in os.walk(path):
+        for dr in dirs:
+            count += len(glob.glob(os.path.join(r, dr + "/*")))
+        for file in tqdm(files):
+            im = imread(os.path.join(r, file), -1)
+            acc_var += (1. / (count-1)) * ((im-ds_mean)**2).sum()
+
+    dataset_std = np.sqrt(acc_var)
+    return dataset_std
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default='data/PA_512_16/M_Adult/train/', help='path to training set')
+    parse.add_argument('--mean', required=True, help='mean value of dataset')
+
+    args = parser.parse_args()
+    dataset_std = get_dataset_std(args.path, args.mean)
+    print(ds_mean)
+
+
+if __name__ == '__main__':
+    main()
