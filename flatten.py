00647bfa2a0168422526c7cde3b4292b07ba1bb4
Yaron Blinder <YaronBlinder@gmail.com>
2017-12-12 22:37:12 +0200

    added flatten function to turn RGB to grayscale in 16bit

--- flatten.py
+++ flatten.py
@@ -20,10 +20,10 @@ def flatten(path, flat_path):
                     flat_im,
                     mode='L',
                     info={
-                        width:flat_im.shape[0],
-                        height:flat_im.shape[1],
-                        grayscale:True,
-                        bitdepth:16}
+                        'width':flat_im.shape[0],
+                        'height':flat_im.shape[1],
+                        'grayscale':True,
+                        'bitdepth':16}
                 ).save(f)
                 # writer.write(f, im_16_2list)
 
