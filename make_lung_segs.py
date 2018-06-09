0483e457ded249c1fd53c2f6b25de03dc9e5bac5
Yaron Blinder <YaronBlinder@gmail.com>
2018-06-09 13:40:18 +0300

    correct path

--- make_lung_segs.py
+++ make_lung_segs.py
@@ -11,14 +11,14 @@ abnormal_files = glob(data_folder+'2/*')
 for i in tqdm(range(10)):
     normal_path = normal_files[i]
     normal_filename = normal_path.split('/')[-1]
-    seg_norm_im = seg_image(normal_path, Nmask_path='lung_seg/Nmask.png')
+    seg_norm_im = seg_image(normal_path, Nmask_path='lung_seg/nmask.png')
     segmented_normal_path = data_folder + 'lung_seg/1/' + normal_filename
     # segmented_normal_path = 'images/segmented/normal/{}'.format(normal_images[i])
     imageio.imwrite(segmented_normal_path, seg_norm_im)
 
     abnormal_path = abnormal_files[i]
     abnormal_filename = abnormal_path.split('/')[-1]
-    seg_abnorm_im = seg_image(abnormal_path, Nmask_path='lung_seg/Nmask.png')
+    seg_abnorm_im = seg_image(abnormal_path, Nmask_path='lung_seg/nmask.png')
     segmented_normal_path = data_folder + 'lung_seg/2/' + abnormal_filename
     # segmented_abnormal_path = 'images/segmented/abnormal/{}'.format(abnormal_images[i])
     imageio.imwrite(segmented_abnormal_path, seg_abnorm_im)
