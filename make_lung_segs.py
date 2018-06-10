e1461c10ac8befa54ff55e0b30a980e583c46bef
Yaron Blinder <YaronBlinder@gmail.com>
2018-06-10 10:56:22 +0300

    faster version

--- make_lung_segs.py
+++ make_lung_segs.py
@@ -14,7 +14,7 @@ abnormal_files = glob(data_folder+'2/*')
 for i in tqdm(range(10)):
     normal_path = normal_files[i]
     normal_filename = normal_path.split('/')[-1]
-    print(normal_path)
+    # print(normal_path)
     normal_image = cv2.imread(normal_path, 0)
     seg_norm_im = faster_seg_image(normal_image, Nmask)
     segmented_normal_path = data_folder + 'lung_seg/1/' + normal_filename
@@ -23,7 +23,7 @@ for i in tqdm(range(10)):
 
     abnormal_path = abnormal_files[i]
     abnormal_filename = abnormal_path.split('/')[-1]
-    print(abnormal_path)
+    # print(abnormal_path)
     abnormal_image = cv2.imread(abnormal_path, 0)
     seg_abnorm_im = faster_seg_image(abnormal_image, Nmask)
     segmented_abnormal_path = data_folder + 'lung_seg/2/' + abnormal_filename
