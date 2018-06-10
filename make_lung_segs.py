fccf5695b08e6f50d136f7491e6ea157a0778eec
Yaron Blinder <YaronBlinder@gmail.com>
2018-06-10 08:24:42 +0300

    devectorize

--- make_lung_segs.py
+++ make_lung_segs.py
@@ -6,7 +6,7 @@ from cv2 import imread
 
 
 Nmask = (imread('lung_seg/nmask.png', 0)/255).astype('uint8')
-data_folder = '/Radical_data/data/CXR8/all/test/'
+data_folder = '/Radical_data/data/cxr8/all/'
 
 normal_files = glob(data_folder+'1/*')
 abnormal_files = glob(data_folder+'2/*')
@@ -14,6 +14,7 @@ abnormal_files = glob(data_folder+'2/*')
 for i in tqdm(range(10)):
     normal_path = normal_files[i]
     normal_filename = normal_path.split('/')[-1]
+    print(normal_path)
     normal_image = cv2.imread(normal_path, 0)
     seg_norm_im = seg_image(normal_image, Nmask)
     segmented_normal_path = data_folder + 'lung_seg/1/' + normal_filename
@@ -22,6 +23,7 @@ for i in tqdm(range(10)):
 
     abnormal_path = abnormal_files[i]
     abnormal_filename = abnormal_path.split('/')[-1]
+    print(abnormal_path)
     abnormal_image = cv2.imread(abnormal_path, 0)
     seg_abnorm_im = seg_image(abnormal_path, Nmask)
     segmented_abnormal_path = data_folder + 'lung_seg/2/' + abnormal_filename
